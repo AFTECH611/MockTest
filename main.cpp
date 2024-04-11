da4842c7f09e8386f8a4b380da90211177871a87
nguyend8230 <nguyend8230@gmail.com>
2024-04-11 16:20:30 +0700

    added new hotels

--- main.cpp
+++ main.cpp
@@ -2,7 +2,6 @@
 
 int main() {
     Application::getApplication().run();
-    // DatabaseManager::appendFile("test.txt", Utility::strCommaD() + Utility::strOpenBracketD() + Utility::strCloseBracketD());
 
     return 0;
 }
